772b969d7d4ab48482b5797288f7d5d01ca572a3
Nadav Samet <thesamet@gmail.com>
2017-09-04 07:37:25 -0700

    TypeRegistyr: track which files has been seen.

--- src/main/scala/com/trueaccord/scalapb/json/JsonFormat.scala
+++ src/main/scala/com/trueaccord/scalapb/json/JsonFormat.scala
@@ -64,19 +64,24 @@ case class FormatRegistry(
   *
   * You need to
   */
-case class TypeRegistry(companions: Map[String, GenericCompanion] = Map.empty) {
+case class TypeRegistry(companions: Map[String, GenericCompanion] = Map.empty, private val filesSeen: Set[String]=Set.empty) {
   def addMessage[T <: GeneratedMessage with Message[T]](implicit cmp: GeneratedMessageCompanion[T]): TypeRegistry = {
     addMessageByCompanion(cmp)
   }
 
   def addFile(file: GeneratedFileObject): TypeRegistry = {
-    val withDeps: TypeRegistry =
-      file.dependencies.foldLeft(this)((r, f) => r.addFile(f))
+    if (filesSeen.contains(file.scalaDescriptor.fullName)) this else {
+      val withFileSeen = copy(filesSeen = filesSeen + (file.scalaDescriptor.fullName))
 
-    file.messagesCompanions.foldLeft(withDeps)((r, mc) => r.addMessageByCompanion(mc.asInstanceOf[GenericCompanion]))
+      val withDeps: TypeRegistry =
+        file.dependencies.foldLeft(withFileSeen)((r, f) => r.addFile(f))
+
+      file.messagesCompanions.foldLeft(withDeps)((r, mc) => r.addMessageByCompanion(mc.asInstanceOf[GenericCompanion]))
+    }
   }
 
   def addMessageByCompanion(cmp: GenericCompanion): TypeRegistry = {
+    // TODO: need to add contained file to follow JsonFormat
     val withNestedMessages =
       cmp.nestedMessagesCompanions.foldLeft(this)((r, mc) => r.addMessageByCompanion(mc.asInstanceOf[GenericCompanion]))
     copy(companions = withNestedMessages.companions + ((TypeRegistry.TypePrefix + cmp.scalaDescriptor.fullName) -> cmp))
