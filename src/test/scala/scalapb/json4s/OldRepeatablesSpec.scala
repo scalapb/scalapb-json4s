977566f833602147cfd169c753fef369aba473ac
Fei Xie <fei.xie-extern@moia.io>
2021-04-08 16:46:29 +0200

    add the proto and downgrade the scalapb compiler plugin to demo the problem

--- src/test/scala/scalapb/json4s/OldRepeatablesSpec.scala
+++ src/test/scala/scalapb/json4s/OldRepeatablesSpec.scala
@@ -1,12 +1,12 @@
 package scalapb.json4s
 
-import generated.oldRepeatables._
+import jsontest.oldRepeatables._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 
 class OldRepeatablesSpec extends AnyFlatSpec with Matchers {
   "JsonFormat" should "serialize some old generated message with repeatables" in {
-    val oldRepeatablesTest = OldRepeatablesTest(repeatedStrings = Seq.empty)
+    val oldRepeatablesTest = OldRepeatablesTest(strings = Seq.empty)
     val json = JsonFormat.toJson(oldRepeatablesTest)
     JsonFormat.fromJson[OldRepeatablesTest](json) mustBe oldRepeatablesTest
   }
