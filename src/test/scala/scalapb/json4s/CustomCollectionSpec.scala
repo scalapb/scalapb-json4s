3cd0d8470fee870defd347af669e9c1d18bb98a3
Fei Xie <fei.xie-extern@moia.io>
2021-04-09 15:30:12 +0200

    use the immutable Seq explicitly for scala 2.12

--- src/test/scala/scalapb/json4s/CustomCollectionSpec.scala
+++ src/test/scala/scalapb/json4s/CustomCollectionSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.matchers.must.Matchers
 
 class CustomCollectionSpec extends AnyFlatSpec with Matchers {
   "JsonFormat" should "serialize/deserialize the custom collection message" in {
-    val googleWrapped = GoogleWrapped(strings = Seq.empty)
+    val googleWrapped = GoogleWrapped(strings = collection.immutable.Seq.empty)
     val json = JsonFormat.toJson(googleWrapped)
     JsonFormat.fromJson[GoogleWrapped](json) mustBe googleWrapped
   }
